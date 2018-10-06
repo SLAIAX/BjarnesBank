8fb95858eb16c302b436ff7e4dddf999d565879f
SLAIAX <1burgerboys@gmail.com>
2018-10-06 15:23:30 +1300

    WORKING

--- web/src/model/AccountModel.php
+++ web/src/model/AccountModel.php
@@ -273,7 +273,7 @@ class AccountModel extends Model
         }
 
 
-        if ($result = $this->db->query("SELECT * FROM `user` WHERE `Username` = $username;")) {
+        if ($result = $this->db->query("SELECT * FROM `user` WHERE `Username` = '$username';")) {
              
             $accounts = $result->fetch_assoc();
             if($accounts['Username'] != ""){
