3eb22324862d0b4427221d42ba9a08f5000a6d84
ZLamb101 <zanelamb@live.com>
2018-10-06 15:14:24 +1300

    a2

--- web/src/model/bankAccountModel.php
+++ web/src/model/bankAccountModel.php
@@ -207,7 +207,7 @@ class bankAccountModel extends Model
             throw new \UnexpectedValueException();
             //throw incomplete data
         }
-      //  echo "$accname, $ID";
+    
         if ($result = $this->db->query("SELECT * FROM `account` WHERE `AccountName` = '$accname' and `UserID` = '$ID';")) {
             // throw new ACCOUNT ALREADY EXISTS
             $accounts = $result->fetch_assoc();
